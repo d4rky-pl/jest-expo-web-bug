9ab098c6fac7dd35ba27afe6bbce5d3ad1e0d0c9
Michal Matyas <github@higher.lv>
2020-06-29 11:12:36 +0200

    Add jest and jest-expo

---
+++ Image.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Image } from 'react-native'
+import TestRenderer from 'react-test-renderer';
+
+const TestComponent = () => <Image source={require('./assets/splash.png')} />
+
+it('does not crash', () => {
+  const component = TestRenderer.create(<TestComponent />)
+  expect(true).toBe(true)
+})
